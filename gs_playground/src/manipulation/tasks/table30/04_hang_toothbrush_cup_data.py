d6f1b44fbe6805a5df85bfa419fdf2f4f240cc86
xyys2003 <2715611906@qq.com>
2026-01-19 17:01:36 +0800

    fix_task4

--- gs_playground/src/manipulation/tasks/table30/04_hang_toothbrush_cup_data.py
+++ gs_playground/src/manipulation/tasks/table30/04_hang_toothbrush_cup_data.py
@@ -349,8 +349,8 @@ class HangToothbrushCupCollector:
             self.video_writers[env_id].close()
             self.video_writers[env_id] = None
         tmp_path = self._tmp_video_paths[env_id]
-        # if self.success[env_id] and (self.saved_success < int(self.cfg.data_size)):
-        if True :
+        if self.success[env_id] and (self.saved_success < int(self.cfg.data_size)):
+        # if True :
             ep_idx = int(self.saved_success)
             final_video_abs = os.path.join(self.videos_dir, f"episode_{ep_idx:05d}.mp4")
             video_rel_path = f"videos/episode_{ep_idx:05d}.mp4"
